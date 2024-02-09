fd0e9917a75418ce600369d59938654305601e07
abracodeabra <akakiplimo@gmail.com>
2024-02-09 23:19:56 +0300

    improve table appearance - dark mode

--- src/components/presentational/Table.tsx
+++ src/components/presentational/Table.tsx
@@ -3,32 +3,34 @@ import styled from 'styled-components';
 const StyledTable = styled.table`
   width: 100%;
   border-collapse: collapse;
+  color: #ffffff;
+  background-color: #1f1f1f;
 `;
 
 const StyledThead = styled.thead`
-  background-color: #656565;
+  background-color: #333333;
 `;
 
 const StyledTbody = styled.tbody`
-  background-color: #818080;
+  background-color: #1f1f1f;
 `;
 
 const StyledTr = styled.tr`
   &:nth-child(even) {
-    background-color: #656565;
+    background-color: #333333;
   }
 `;
 
 const StyledTh = styled.th`
   padding: 15px;
   text-align: left;
-  border-bottom: 1px solid #ddd;
+  border-bottom: 1px solid #444444;
 `;
 
 const StyledTd = styled.td`
   padding: 15px;
   text-align: left;
-  border-bottom: 1px solid #ddd;
+  border-bottom: 1px solid #444444;
 `;
 
 export { StyledTable, StyledThead, StyledTbody, StyledTr, StyledTh, StyledTd };
